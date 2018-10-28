be77bf0461c9f69aeec08c83142cc0a2ea72d76d
Jan Sobus <sobus.jan@gmail.com>
2018-10-28 22:22:00 +1000

    Only 1 user can update, success with alert

--- contact.js
+++ contact.js
@@ -3,41 +3,91 @@ if (firebase.auth().currentUser) {
 
         firebase.auth().signOut();
       }
+
+//Getting Elements
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const messagebox = document.getElementById('messagebox');
+const submit = document.getElementById('submit');
+const bottom = document.getElementById('bottom');
+const verify = document.getElementById('verify');
+const reset = document.getElementById('reset');
+const message = document.getElementById('message');
+
+messagebox.style.display='none';
+submit.style.display='none';
+reset.style.display='none';
+
 function initApp() {
       // Listening for auth state changes.
       // [START authstatelistener]
       firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                   document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+                  console.log(user);
+                  messagebox.style.display='block';
+                  submit.style.display='block';
+                  reset.style.display='block';
                 } else {
                   document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+                  messagebox.style.display='none';
+                  submit.style.display='none';
+                  reset.style.display='none';
                 }
               });
 }
 window.onload = function() {
       initApp();
     };
-//Getting Elements
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-const submit = document.getElementById('submit');
-
 
 //Create references
 const dbRefObject = firebase.database().ref();
 
-//Submitting function
-function submitData2() {
+verify.addEventListener('click', e=>{
+  var pass = document.getElementById('pass');
+  firebase.auth().signInWithEmailAndPassword('sobus.jan@gmail.com', pass.value).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errormessagebox = error.messagebox;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errormessagebox);
+          }
+          console.log(error);
+
+          // [END_EXCLUDE]
+        });
+});
+
+submit.addEventListener('click', e=>{
   var postData = {
     author: name.value,
     text: message.value
-  };
+    };
+    console.log(name.value);
+    console.log(email.value);
+    console.log(message.value);
 
-  dbRefObject.child(email.value).set(postData);
+          // [END_EXCLUDE]
+      dbRefObject.child(email.value).set(postData).catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errormessagebox = error.messagebox;
+              // [START_EXCLUDE]
+              if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+              } else {
+                alert(errormessagebox);
+              }
+              console.log(error);
 
-  }
-
+              // [END_EXCLUDE]
+            });
+      alert('Your data was added');
+    });
+/*
 function submitData(){
   firebase.auth().signOut();
   var pass = document.getElementById('pass');
@@ -45,12 +95,12 @@ function submitData(){
   firebase.auth().signInWithEmailAndPassword('sobus.jan@gmail.com', pass.value).catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
-          var errorMessage = error.message;
+          var errormessagebox = error.messagebox;
           // [START_EXCLUDE]
           if (errorCode === 'auth/wrong-password') {
             alert('Wrong password.');
           } else {
-            alert(errorMessage);
+            alert(errormessagebox);
           }
           console.log(error);
 
@@ -59,7 +109,7 @@ function submitData(){
 
         var postData = {
           author: name.value,
-          text: message.value
+          text: messagebox.value
         };
         if (firebase.auth().currentUser) {
             dbRefObject.child(email.value).set(postData);
@@ -69,3 +119,4 @@ function submitData(){
 
     firebase.auth().signOut();
 }
+*/
