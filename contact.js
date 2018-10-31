d9bcf5a66007d115a4ba58b4fcea6d17adcc5d80
J <sobus.jan@gmail.com>
2018-10-31 21:45:23 +1000

    Added remaining forms

--- contact.js
+++ contact.js
@@ -16,21 +16,42 @@ const message = document.getElementById('message');
 const adults_field = document.getElementById('adults_field');
 const number_adults = document.getElementById('number_adults');
 const adults_names = document.getElementsByName('adult');
-console.log(adults_names);
+const kids_field = document.getElementById('kids_field');
+const number_kids = document.getElementById('number_kids');
+const kids_names = document.getElementsByName('kid');
+const infants_field = document.getElementById('infants_field');
+const number_infants = document.getElementById('number_infants');
+const infants_names = document.getElementsByName('infant');
+const after_field = document.getElementById('after_field');
+const transport_in = document.getElementById('transport_in');
+const transport_field = document.getElementById('transport_field');
 
 
+
+//Hiding some elements before verification
 messagebox.style.display='none';
 submit.style.display='none';
 reset.style.display='none';
 adults_field.style.display='none';
+kids_field.style.display='none';
+infants_field.style.display='none';
+after_field.style.display='none';
+transport_field.style.display='none';
 
 for (var i = 0; i < adults_names.length; ++i)
     {
-
             adults_names[i].style.display = 'none';
-
+    }
+for (var i = 0; i < infants_names.length; ++i)
+    {
+            infants_names[i].style.display = 'none';
+    }
+for (var i = 0; i < kids_names.length; ++i)
+    {
+            kids_names[i].style.display = 'none';
     }
 
+//Function listening for auth changes and showing/hiding appropriate fields
 function initApp() {
       // Listening for auth state changes.
       // [START authstatelistener]
@@ -42,6 +63,10 @@ function initApp() {
                   submit.style.display='block';
                   reset.style.display='block';
                   adults_field.style.display='block';
+                  kids_field.style.display='block';
+                  infants_field.style.display='block';
+                  after_field.style.display='block';
+                  transport_field.style.display='block';
 
                 } else {
                 //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
@@ -49,11 +74,16 @@ function initApp() {
                   submit.style.display='none';
                   reset.style.display='none';
                   adults_field.style.display='none';
+                  kids_field.style.display='none';
+                  infants_field.style.display='none';
+                  after_field.style.display='none';
+                  transport_field.style.display='none';
 
                 }
               });
 }
 
+//Function giving proper number of fields depending on selection
 number_adults.onchange = function() {
 
     for (var i = 0; i < adults_names.length; ++i)
@@ -67,13 +97,41 @@ number_adults.onchange = function() {
     }
 
 }
+
+number_kids.onchange = function() {
+
+    for (var i = 0; i < kids_names.length; ++i)
+    {
+        if(i < number_kids.value){
+            kids_names[i].style.display = 'inline';
+
+        } else {
+            kids_names[i].style.display = 'none';
+        }
+    }
+
+}
+number_infants.onchange = function() {
+
+    for (var i = 0; i < infants_names.length; ++i)
+    {
+        if(i < number_infants.value){
+            infants_names[i].style.display = 'inline';
+
+        } else {
+            infants_names[i].style.display = 'none';
+        }
+    }
+
+}
 window.onload = function() {
       initApp();
     };
 
-//Create references
+//Create database reference
 const dbRefObject = firebase.database().ref();
 
+//Adding action to the verification button
 verify.addEventListener('click', e=>{
   var pass = document.getElementById('pass');
   firebase.auth().signInWithEmailAndPassword('sobus.jan@gmail.com', pass.value).catch(function(error) {
@@ -92,6 +150,7 @@ verify.addEventListener('click', e=>{
         });
 });
 
+//Adding action to submission button
 submit.addEventListener('click', e=>{
   var postData = {
     author: name.value,
